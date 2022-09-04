f25708e7ca58a98a3ef6b0ccf66993d91ed79fef
Anandu <ananduprajesh@gmail.com>
2022-09-04 15:05:48 +0530

    fixing error in hosting

--- pages/album.js
+++ pages/album.js
@@ -89,9 +89,9 @@ function Album() {
             </div>
             <Footer />
 
-            <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-            <script type="text/javascript" src="js/bootstrap.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+            <script type="text/javascript" src="js/jquery-3.4.1.min.js" async></script>
+            <script type="text/javascript" src="js/bootstrap.js" async></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" async></script>
 
         </div>
     )
